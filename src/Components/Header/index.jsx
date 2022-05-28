11551091fc27143fbfc48ed12d2151aba562145e
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-05-29 03:50:58 +0530

    Change styles

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import CV from "./CV"
-import HeaderSocials from './HeaderSocials'
 import ME from "../../Asserts/SasinduNanayakkara.jpg"
+import HeaderSocials from './HeaderSocials'
 function Header() {
   return (
-    <header className='h-screen pt-28 overflow-hidden'>
-      <div className="container text-center h-full relative">
+    
+    <header className='h-screen pt-12 overflow-x-hidden'>
+      <div className="container text-center h-48">
         <h5>Hello I'm</h5>
         <h1>Sasindu Nanayakkara</h1>
         <h5 className="text-slate-500">Passionate Full Stack Developer</h5>
         <CV />
-        <HeaderSocials />
 
-        <div className='bg-slate-900 w-80 h-96 ml-10 absolute left-2/4 -translate-x-1/2 mt-16 rounded-t-full rounded-b-full rounded-l-none rounded-r-none from-amber-400'>
+        <div className='bg-slate-900 w-80 h-96 ml-7 absolute left-2/4 -translate-x-1/2 mt-12'>
           <img src={ME} alt='profile pic' />
+        <HeaderSocials/>
         </div>
 
-        <a href='#contact' className=''>Scroll Down</a>
+        <a href='#contact' className='absolute right-32  bottom-20 rotate-90 font-light text-sm'>Scroll Down</a>
       </div>
     </header>
+    
   )
 }
 
