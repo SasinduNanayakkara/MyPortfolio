62dd7e2e78d02cfc584183dabdb89c7bdca1a52e
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-05-29 03:50:37 +0530

    Chnage CSS

--- src/Components/Header/HeaderSocials.jsx
+++ src/Components/Header/HeaderSocials.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import {BsLinkedin, BsGithub, BsFacebook} from "react-icons/bs"
 function HeaderSocials() {
   return (
-    <div className='flex flex-col items-center gap-3.5 absolute left-0 bottom-12 ml-64'>
-        <a href='https://www.linkedin.com/in/sasindu-nanayakkara/' target="_blank" rel="noreferrer" className='w-5 h-5'><BsLinkedin/></a>
-        <a href='https://github.com/SasinduNanayakkara' target="_blank" rel="noreferrer" className='w-5 h-5'><BsGithub/></a>
-        <a href='https://www.facebook.com/profile.php?id=100007102998619' target="_blank" rel="noreferrer" className='w-5 h-5'><BsFacebook/></a>
+    <div className='flex justify-center mt-5 mr-6'>
+        <a href='https://www.linkedin.com/in/sasindu-nanayakkara/' target="_blank" rel="noreferrer" className='pr-10 '><BsLinkedin size={30}/></a>
+        <a href='https://github.com/SasinduNanayakkara' target="_blank" rel="noreferrer" className='pr-10 '><BsGithub size={30}/></a>
+        <a href='https://www.facebook.com/profile.php?id=100007102998619' target="_blank" rel="noreferrer" className='pr-10 '><BsFacebook size={30}/></a>
 
     </div>
   )
