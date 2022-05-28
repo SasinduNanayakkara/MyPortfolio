433d2abc30d82355d15a055c7b52d6b31be9cced
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-05-29 03:49:05 +0530

    Change CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import About from "./Components/About";
-import Contact from "./Components/Contatct";
+import Contact from "./Components/Contact";
 import Experience from "./Components/Experience";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
@@ -8,7 +8,7 @@ import Portfolio from "./Components/Portfolio";
 import "./index.css"
 function App() {
   return (
-    <div className="bg-slate-900 text-white m-0 p-0 border-0 outline-0 box-border list-none no-underline">
+    <div className="bg-slate-900 scroll-smooth text-white m-0 p-0 border-0 outline-0 box-border list-none no-underline">
       <Header />
       <Nav />
       <About />
