1a30059003d7a2556b62e5836c5bc551395ace49
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-05-28 15:36:16 +0530

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryBtnColor: "var(--primary-btn-color)",
+        primaryBtnHoverColor: "var(--primary-btn-hover-color)",
+        secondaryBtnColor: "var(--secondary-btn-color)",
+        secondaryBtnHoverColor: "var(--secondary-btn-hover-color)",
+        secondaryBgColor: "var(--secondary-bg-color)",
+        divBgColor: "var(--div-bg-color)",
+        divSecondaryBgColor: "var(--div-secondary-bg-color)",
+      }
+    },
+  },
+  plugins: [],
+}
