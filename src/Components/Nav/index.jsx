d28349bb1b8d08ee658a1a31f6be4811b05efc60
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-05-29 03:51:12 +0530

    Implement nav bar

--- src/Components/Nav/index.jsx
+++ src/Components/Nav/index.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {AiOutlineHome, AiOutlineUser} from "react-icons/ai"
+import {BiBook, BiMessageAltDetail} from "react-icons/bi"
+import {IoMdPaper} from "react-icons/io"
+
 
 function Nav() {
+
+  const [active, setActive] = useState('#')
   return (
-    <div>Nav</div>
+    <nav className='bg-black bg-opacity-25 w-max block py-2 px-7 z-20 fixed left-1/2 -translate-x-1/2 bottom-3 flex gap-3 rounded-full backdrop-blur-lg'>
+      {/*eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+      <a href='#' onClick={() => setActive("#")} className={active === "#" ? "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20 bg-slate-500" : "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20"}><AiOutlineHome size={20}/></a>
+      <a href='#about' onClick={() => setActive("#about")} className={active === "#about" ? "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20 bg-slate-500" : "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20"}><AiOutlineUser size={20}/></a>
+      <a href='#experience' onClick={() => setActive("#experience")} className={active === "#experience" ? "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20 bg-slate-500" : "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20"}><BiBook size={20}/></a>
+      <a href='#portfolio' onClick={() => setActive("#portfolio")} className={active === "#portfolio" ? "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20 bg-slate-500" : "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20"}><IoMdPaper size={20}/></a>
+      <a href='#contact' onClick={() => setActive("#contact")} className={active === "#contact" ? "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20 bg-slate-500" : "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20"}><BiMessageAltDetail size={20}/></a>
+    </nav>
   )
 }
 
