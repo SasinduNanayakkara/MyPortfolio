6a1945715fff556eb76fb8d5ebaa6d0b578d5d2f
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-05-30 16:01:46 +0530

    Implement project data

---
+++ src/Components/Portfolio/data.js
@@ -0,0 +1,56 @@
+export const techData = [
+    {
+        id: 1,
+        title: 'Learning Management System',
+        image: "../../Asserts/braintech.png",
+        description: "A group project of Learning Management System for BrainTech Institute",
+        techName : "MERN Stack",
+        color : "text-fuschia-600",
+        url : "https://github.com/SasinduNanayakkara/ITP-frontEnd/tree/development"
+    },
+    {
+        id: 2,
+        title: 'E-Commerce Mobile App',
+        image: "../../Asserts/mobile app.png",
+        description: "A group project of E-Commerce Android Application",
+        techName : "Java",
+        color : "text-orange-600",
+        url : "https://github.com/SasinduNanayakkara/E-Commerce-Applicaiton"
+    },
+    {
+        id: 3,
+        title: 'Python Programming',
+        image: "../../Asserts/python image.png",
+        description: "Basic python, GUI, OOP, File Handling, API, Selenium and More...",
+        techName : "Python",
+        color : "text-yellow-300",
+        url : "https://github.com/SasinduNanayakkara/Python"
+    },
+    {
+        id: 4,
+        title: 'Face Detector',
+        image: "../../Asserts/face detection.jpeg",
+        description: "Facial Recognition Attendance System using OpenCV",
+        techName : "Python",
+        color : "text-yellow-300",
+        url : "https://github.com/SasinduNanayakkara/Facial-Recognition-Attendence-Project"
+    },
+    {
+        id: 5,
+        title: 'Terraform with AWS',
+        image: "../../Asserts/terraform.png",
+        description: "A Dynamic Terraform project to deploy a web server on AWS",
+        techName : "Terraform(HCL)",
+        color : "text-indigo-800",
+        url : "https://github.com/SasinduNanayakkara/AWS-Terraform-Module"
+    },
+    {
+        id: 6,
+        title: 'Blog Application',
+        image: "../../Asserts/blog app.png",
+        description: "Simple Blog Application using MERN stack and Google OAuth",    
+        techName : "MERN Stack",
+        color : "text-fuschia-600",
+        url : "https://github.com/SasinduNanayakkara/Oauth-demo-FrontEnd-"
+    }
+]
