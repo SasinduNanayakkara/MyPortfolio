3dd06433b83f5ded9c5e27d996e96cdcdf0d8444
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-06-01 01:55:06 +0530

    Add new hover messages

--- src/Components/Nav/index.jsx
+++ src/Components/Nav/index.jsx
@@ -1,8 +1,9 @@
 import React, {useState} from 'react'
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
 import {AiOutlineHome, AiOutlineUser} from "react-icons/ai"
 import {BiBook, BiMessageAltDetail} from "react-icons/bi"
 import {IoMdPaper} from "react-icons/io"
-
 
 function Nav() {
 
@@ -10,11 +11,11 @@ function Nav() {
   return (
     <nav className='bg-black bg-opacity-25 w-max block py-2 px-7 z-20 fixed left-1/2 -translate-x-1/2 bottom-3 flex gap-3 rounded-full backdrop-blur-lg'>
       {/*eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-      <a href='#' onClick={() => setActive("#")} className={active === "#" ? "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20 bg-slate-500" : "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20"}><AiOutlineHome size={20}/></a>
-      <a href='#about' onClick={() => setActive("#about")} className={active === "#about" ? "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20 bg-slate-500" : "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20"}><AiOutlineUser size={20}/></a>
-      <a href='#experience' onClick={() => setActive("#experience")} className={active === "#experience" ? "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20 bg-slate-500" : "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20"}><BiBook size={20}/></a>
-      <a href='#portfolio' onClick={() => setActive("#portfolio")} className={active === "#portfolio" ? "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20 bg-slate-500" : "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20"}><IoMdPaper size={20}/></a>
-      <a href='#contact' onClick={() => setActive("#contact")} className={active === "#contact" ? "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20 bg-slate-500" : "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20"}><BiMessageAltDetail size={20}/></a>
+      <Tippy content="Home"><a href='#' onClick={() => setActive("#")} className={active === "#" ? "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20 bg-slate-500" : "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20"}><AiOutlineHome size={20}/></a></Tippy>
+      <Tippy content="About Me"><a href='#about' onClick={() => setActive("#about")} className={active === "#about" ? "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20 bg-slate-500" : "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20"}><AiOutlineUser size={20}/></a></Tippy>
+      <Tippy content="My Experience"><a href='#experience' onClick={() => setActive("#experience")} className={active === "#experience" ? "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20 bg-slate-500" : "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20"}><BiBook size={20}/></a></Tippy>
+      <Tippy content="My Projects"><a href='#portfolio' onClick={() => setActive("#portfolio")} className={active === "#portfolio" ? "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20 bg-slate-500" : "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20"}><IoMdPaper size={20}/></a></Tippy>
+      <Tippy content="Contact Me"><a href='#contact' onClick={() => setActive("#contact")} className={active === "#contact" ? "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20 bg-slate-500" : "bg-transparent p-4 rounded-2xl flex text-base hover:bg-amber-500 hover:bg-opacity-20"}><BiMessageAltDetail size={20}/></a></Tippy>
     </nav>
   )
 }
