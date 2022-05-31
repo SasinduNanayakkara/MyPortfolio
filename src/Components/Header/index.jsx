a70854aa0e6973b480320ed86738bdbe6ab42a95
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-06-01 01:37:21 +0530

    Add new class names

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -3,18 +3,19 @@ import CV from "./CV"
 import ME from "../../Asserts/SasinduNanayakkara.jpg"
 import HeaderSocials from './HeaderSocials'
 import "./index.css"
+import "../../index.css"
 function Header() {
   return (
     
-    <header className='head h-screen pt-12 overflow-x-hidden'>
-      <div className="container header__container text-center h-48">
-        <h5>Hello I'm</h5>
-        <h1>Sasindu Nanayakkara</h1>
-        <h5 className="text-slate-500">Passionate Full Stack Developer</h5>
-        <CV />
+    <header className='head h-screen pt-12 overflow-hidden'>
+      <div className="container header__container text-center h-full relative">
+        <h5 className='text-hello'>Hello I'm</h5>
+        <h1 className='title'>Sasindu Nanayakkara</h1>
+        <h5 className="title-desc text-slate-500">Passionate Full Stack Developer</h5>
+        <CV/>
 
-        <div className='bg-slate-900 w-80 h-96 ml-7 absolute left-2/4 -translate-x-1/2 mt-12'>
-          <img src={ME} alt='profile pic' className='rounded-full'/>
+        <div className='image-div bg-slate-900 w-80 h-96 ml-7 absolute left-2/4 -translate-x-1/2 mt-12'>
+          <img src={ME} alt='profile pic' className='profile-image rounded-full'/>
         <HeaderSocials/>
         </div>
       </div>
