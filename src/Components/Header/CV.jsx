1347e5a6b941eb9915569a4b6be84c71af05d92d
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-06-01 01:35:13 +0530

    Add new classes

--- src/Components/Header/CV.jsx
+++ src/Components/Header/CV.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import CVFile from "../../Asserts/Sasindu Nanayakkara - CV.pdf"
+import "../../index.css"
 function CV() {
   return (
-    <div className='mt-10 flex gap-5 justify-center'>
-        <a href={CVFile} download className="btn">Download CV</a>
-        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+    <div className='cv-div mt-10 flex gap-5 justify-center lg:mt-10'>
+        <a href={CVFile} download className="btn cv-btn">Download CV</a>
+        <a href='#contact' className='btn btn-primary contact-btn'>Let's Talk</a>
     </div>
   )
 }
