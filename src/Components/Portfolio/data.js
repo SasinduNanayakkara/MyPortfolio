161338a184ff617b5528275284328eeb5acd4d39
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-08-30 22:05:30 +0530

    Update the project data

--- src/Components/Portfolio/data.js
+++ src/Components/Portfolio/data.js
@@ -5,26 +5,26 @@ export const techData = [
         image: "../../Asserts/braintech.png",
         description: "A group project of Learning Management System for BrainTech Institute",
         techName : "MERN Stack",
-        color : "text-fuschia-600",
+        color : "text-yellow-300",
         url : "https://github.com/SasinduNanayakkara/ITP-frontEnd/tree/development"
     },
     {
         id: 2,
-        title: 'E-Commerce Mobile App',
-        image: "../../Asserts/mobile app.png",
-        description: "A group project of E-Commerce Android Application",
-        techName : "Java",
+        title: 'Crypto Trade',
+        image: "../../Asserts/crypto-trade.jpg",
+        description: "A react native application help to check the statistics of the latest and popular cryptocurrencies",
+        techName : "React Native",
         color : "text-orange-600",
-        url : "https://github.com/SasinduNanayakkara/E-Commerce-Applicaiton"
+        url : "https://github.com/SasinduNanayakkara/Crypto-Trade"
     },
     {
         id: 3,
-        title: 'Python Programming',
-        image: "../../Asserts/python image.png",
-        description: "Basic python, GUI, OOP, File Handling, API, Selenium and More...",
-        techName : "Python",
+        title: 'A Chat',
+        image: "../../Asserts/chat-app.png",
+        description: "Instance chat application developed using react, express, tailwind CSS and socket.io",
+        techName : "MERN Stack",
         color : "text-yellow-300",
-        url : "https://github.com/SasinduNanayakkara/Python"
+        url : "https://github.com/SasinduNanayakkara/Achat"
     },
     {
         id: 4,
@@ -32,25 +32,25 @@ export const techData = [
         image: "../../Asserts/face detection.jpeg",
         description: "Facial Recognition Attendance System using OpenCV",
         techName : "Python",
-        color : "text-yellow-300",
+        color : "text-cyan-400",
         url : "https://github.com/SasinduNanayakkara/Facial-Recognition-Attendence-Project"
     },
     {
         id: 5,
-        title: 'Terraform with AWS',
-        image: "../../Asserts/terraform.png",
-        description: "A Dynamic Terraform project to deploy a web server on AWS",
-        techName : "Terraform(HCL)",
+        title: 'Terraform on Azure',
+        image: "../../Asserts/terraform-azure.png",
+        description: "A Dynamic Terraform script to create some resources on Azure",
+        techName : "Terraform (HCL)",
         color : "text-indigo-800",
-        url : "https://github.com/SasinduNanayakkara/AWS-Terraform-Module"
+        url : "https://github.com/SasinduNanayakkara/Terraform-on-Azure"
     },
     {
         id: 6,
-        title: 'Blog Application',
-        image: "../../Asserts/blog app.png",
-        description: "Simple Blog Application using MERN stack and Google OAuth",    
-        techName : "MERN Stack",
-        color : "text-fuschia-600",
-        url : "https://github.com/SasinduNanayakkara/Oauth-demo-FrontEnd-"
+        title: 'S Coin',
+        image: "../../Asserts/scoin.webp",
+        description: "A basic crypto token with transfer features developed using react, motoko and dfinity",    
+        techName : "Motoko",
+        color : "text-emerald-400",
+        url : "https://github.com/SasinduNanayakkara/SCoin"
     }
 ]
